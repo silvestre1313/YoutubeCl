3a2be88fa47402e245c18f2dc74e75de2a4a4037
Patrick <patrick.silvestre13@hotmail.com>
2021-11-30 20:10:35 -0300

    Configurando retrofit e youtube

---
+++ app/src/main/java/com/example/youtube/helper/YoutubeConfig.java
@@ -0,0 +1,9 @@
+package com.example.youtube.helper;
+
+public class YoutubeConfig {
+
+    public static String CANAL_ID = "";
+    public static String CHAVE_YOUTUBE_API = "AIzaSyDDSA0i4UbU6xundpYjAcZ0HLT3pZ55jtY";
+    public static String URL_BASE = "";
+
+}
